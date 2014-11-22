5ed594df06e155c1ca37afa097f5b1da93a2f8b1
KishorNaik <kishor.naik011@live.com>
2014-11-23 01:27:06 +0530

    StateManager - 22.11.2014

--- SolHelperCSharp/Helper/StateMangement/StateManagerHelper.cs
+++ SolHelperCSharp/Helper/StateMangement/StateManagerHelper.cs
@@ -62,6 +62,115 @@ namespace Helper.StateMangement
             }
         }
 
+        /// <summary>
+        /// This method is used for store data with specified the key. 
+        /// </summary>
+        /// <typeparam name="T">Specify the name of type</typeparam>
+        /// <param name="Key">Specify the name of key</param>
+        /// <param name="Value">Specify the name of value</param>
+        public static void Add<T>(String Key, T Value)
+        {
+            try
+            {
+                // Check key and value is null or not
+                if (Key == String.Empty && Value == null) throw new ArgumentNullException();
+
+                // Add a value with key.
+                DicStateObj.Add(Key, Value);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This method used for to get a value by specifying key.
+        /// </summary>
+        /// <param name="Key">Specify the name of key</param>
+        /// <returns>Object</returns>
+        public static Object GetValue(String Key)
+        {
+            try
+            {
+                // Check key is empty or not
+                if (Key != String.Empty) throw new ArgumentNullException();
+
+                // Get the value based on specifying key
+                return DicStateObj[Key];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+       /// <summary>
+        ///  This method used for to get a value with the specifying key.
+       /// </summary>
+       /// <typeparam name="T">Specify the name of type</typeparam>
+       /// <param name="Key">Specify the name of key</param>
+       /// <returns>T</returns>
+        public static T GetValue<T>(String Key)
+        {
+            T TObj = default(T);
+            try
+            {
+                // Check key is empty or not
+                if (Key != String.Empty) throw new ArgumentNullException();
+
+                TObj =(T) DicStateObj[Key];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return TObj;
+        }
+
+        /// <summary>
+        /// This method used for to remove the value with the specified key.
+        /// </summary>
+        /// <param name="Key">Specify the name of key</param>
+        /// <returns>Boolean</returns>
+        public static Boolean Remove(String Key)
+        {
+            try
+            {
+                // Check the key is emoty or not.
+                if (Key == String.Empty) throw new ArgumentNullException();
+
+                // Check the specified key find in Dic Object.
+                if (DicStateObj.Count(LE => LE.Key == Key) >= 1)
+                {
+                    // if key find then remove the value with the specified key.
+                    return DicStateObj.Remove(Key);
+                }
+                throw new KeyNotFoundException();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This method used for to remove all values and keys.
+        /// </summary>
+        public static void RemoveAll()
+        {
+            try
+            {
+                // Remove all keys and values.
+                DicStateObj.Clear();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
         #endregion
 
         #region Asynchronous Method
@@ -78,6 +187,89 @@ namespace Helper.StateMangement
             {
                 //Async Operation
                 return Task.Run(() => Add(Key, Value));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This method is used for store data Asynchronously.
+        /// </summary>
+        /// <typeparam name="T">Specify the name of type</typeparam>
+        /// <param name="Key">Specify the name of key</param>
+        /// <param name="Value">Specify the name of value</param>
+        /// <returns>Task</returns>
+        public static Task AddAsyanc<T>(String Key, T Value)
+        {
+            try
+            {
+                return Task.Run(() => Add<T>(Key, Value));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This method used for to get a value asynchronously with the specified key.
+        /// </summary>
+        /// <param name="Key">Specify the name of key</param>
+        /// <returns>Object</returns>
+        public static Task<Object> GetValueAsync(String Key)
+        {
+            try
+            {
+                return Task.Run<Object>(() => GetValue(Key));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        ///  This method used for to get a value asynchronously with the specified key.
+        /// </summary>
+        /// <typeparam name="T">Specify the name of type.</typeparam>
+        /// <param name="Key">Specify the name of key</param>
+        /// <returns>T</returns>
+        public static Task<T> GetValueAsync<T>(String Key)
+        {
+            try
+            {
+                return Task.Run<T>(() => GetValue<T>(Key));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This method used for to remove the value asynchronously with the specified key.
+        /// </summary>
+        /// <param name="Key">Specify the name of key</param>
+        /// <returns>Boolean</returns>
+        public static Task<Boolean> RemoveAsync(String Key)
+        {
+            try
+            {
+                return Task.Run<Boolean>(() => Remove(Key));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static Task RemoveAllAsync()
+        {
+            try
+            {
+                return Task.Run(() => RemoveAll());
             }
             catch (Exception ex)
             {
